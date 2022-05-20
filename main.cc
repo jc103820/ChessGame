ee878e121ad4cc6c153f000fe66a5819714ffbd0
jc103820 <jc103820@ohio.edu>
2022-05-20 13:25:53 -0400

    Hello World

---
+++ main.cc
@@ -0,0 +1,12 @@
+/**
+ * @file main.cc
+ * @author Jansen Craft
+ * @brief - Main File For C++ Chess Game
+ * @date 2022-05-20
+ */
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World" << std::endl;
+}
